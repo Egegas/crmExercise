894bc8882d7e60290f450acc7a42f62d003a5f47
egegas <ege150581@gmail.com>
2021-03-10 19:10:02 +0200

    operations

---
+++ contact.db.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const { isNull } = require('util');
+
+o ={
+    name: null, 
+    id: "", 
+    email: "", 
+    Phone: "", 
+    ComapnyID: ""
+}
+
+const contacts = [];
+
+function read(callback) {
+    callback(null, contacts);
+}
+
+function create(contact, callback) {
+    if (contacts.find(c => c.id === contact.id)) {
+        return callback('contact already exist');
+    }
+
+    contacts.push(contact);
+    callback(null, contact);
+}
+
+function update(contact, callback) {
+    const _contact = contacts.find(c => c.id === contact.id);
+    let newcontact = Object.assign(_contact, contact);
+    contacts.splice(contacts.indexOf(_contact), 1, newcontact);
+
+    callback(null, true);
+}
+
+function remove(contactid, callback) {
+    const contact = contacts.find(c => c.id === cocontactid);
+    phones.splice(contacts.indexOf(contact), 1);
+    callback(null, true);
+}
+
+module.exports = {
+    read: read,
+    create: create,
+    update: update,
+    remove: remove
+}
--- index.js
+++ index.js
@@ -1,3 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// my modules imports
+const contactDb = require('./contact.db');
+
+// configurations
+const PORT = 14700;
+const app = express();
+app.use(bodyParser.json());
+
+
+app.get('/contact', function (req, res) {
+    contactDb.read(function (err, contactsData) {
+        if (err) return res.status(500).send();
+
+        res.json(contactsData);
+    });
+});
+
+app.post('/contact', function (req, res) {
+    const contact = req.body;
+    contactDb.create(contact, function (err, contactData) {
+        if (err) return res.status(500).send(err);
+
+        res.status(201).json(contactData);
+    });
+});
+
+app.put('/contact', function (req, res) {
+    const contact = req.body;
+    contactDb.update(contact, function (err, success) {
+        if (err) return res.status(500).send();
+
+        res.status(200).json(success);
+    });
+});
+
+app.delete('/contact', function (req, res) {
+    const { contact_id } = req.query;
+    phoneDb.remove(contact_id, function (err, data) {
+        if (err) return res.status(500).send();
+
+        res.status(204).send();
+    });
+});
+
+app.listen(PORT, () => console.log(`server started at port ${PORT}`));
